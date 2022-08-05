4abe3fa88392786d84e7b5ed03d6a319b85a0112
Yasmin <jasminvk@yandex.ru>
2022-08-05 17:06:20 +0300

    task2

---
+++ task2/Task2.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число для 'a': ");
+var a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число для 'b': ");
+var b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"a = {a}; b = {b} -> max = {Math.Max(a, b)},
+ min = {Math.Min(a, b)}");
