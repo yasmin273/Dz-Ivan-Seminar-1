b194fead7b950fa4e547bd07fe8d85242fe2cd60
Yasmin <jasminvk@yandex.ru>
2022-08-05 17:14:28 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// //Напишите программу, которая на вход принимает число (N),
+//  а на выходе показывает все чётные числа от 1 до N
+
+IReadOnlyList<int> EvenNumbers(int N)
+{
+    var evenNums = new List<int>();
+    for (int i = 2; i <= N; i += 2)
+        evenNums.Add(i);
+
+    return evenNums;
+}
+
+Console.Write("Введите число N: ");
+var N = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"{N} -> {string.Join(", ", EvenNumbers(N))}");
