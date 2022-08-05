7ff64975cbc99e12f0879aa52a87943cfd744616
Yasmin <jasminvk@yandex.ru>
2022-08-05 17:11:45 +0300

    Task6

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// /// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+bool IsEvenNumber(int num) => num % 2 == 0;
+
+Console.Write("Введите число: ");
+var num = int.Parse(Console.ReadLine());
+var result = IsEvenNumber(num) ? "да" : "нет";
+
+Console.WriteLine($"{num} -> {result}");
